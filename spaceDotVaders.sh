e7bb9d77e83295b7180fbbb2d5e6652cfe604532
OGabrielPereira <ogabrielpereira@pm.me>
2022-09-24 23:07:01 -0300

    Moving ship top and bottom

--- spaceDotVaders.sh
+++ spaceDotVaders.sh
@@ -7,35 +7,43 @@ FARRIGHT=50 #Máximo que a nave pode ir(tamanho da tela jogável)
 
 # Movimentação da nave
 ship=25 # Posição inicial da nave(x)
-direction=1
-offset=20
-bottom=20
+bottom=20 # Posição inicial da nave(y)
 
 function drawship
 {
-  tput cup $bottom 0
-  printf "%80s" " "
-  tput cup $bottom $ship
-  shipStyle=("|--|\033[1;31m*\033[0m|--|")
-  echo -en "$shipStyle"
+    clear
+    tput cup $bottom 0
+    printf "%80s" " "
+    tput cup $bottom $ship
+    shipStyle=("|--|\033[1;31m*\033[0m|--|")
+    echo -en "$shipStyle"
 }
 
 drawship
 
+# Looping principal
 while :
 do
-  read -s -n 1 key
-  case "$key" in
-    a)
-       [ $ship -gt 0 ] && ((ship=ship-1))
-	drawship
-	;;
-    d)
-       [ $ship -lt $FARRIGHT ] && ((ship=ship+1))
-	drawship
-	;;
-    q)
-	echo -e "\nEspero vê-lo de novo!" && exit 0
-	;;
+    read -s -n 1 key
+    case "$key" in
+        a)
+            [ $ship -gt 0 ] && ((ship=ship-1))
+	            drawship
+	        ;;
+        d)
+            [ $ship -lt $FARRIGHT ] && ((ship=ship+1))
+	            drawship
+	        ;;
+        w) # Move pra cima
+            [ $bottom -gt 0 ] && ((bottom=bottom-1))
+                drawship
+            ;;
+        s) # Move pra baixo
+            [ $bottom -lt 20 ] && ((bottom=bottom+1))
+                drawship
+            ;;
+        q)
+	        echo -e "\nEspero vê-lo de novo!" && exit 0
+	        ;;
   esac
 done
